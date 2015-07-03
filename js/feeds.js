63a3f5681c515a32b8ce43f98fdc3d1c2a694cf0
Thanigaivelu Mohanraj <mohanraj.t@imaginea.com>
2015-07-03 21:20:57 +0530

    add css changes and font awesome

--- js/feeds.js
+++ js/feeds.js
@@ -36,14 +36,18 @@ Feeds.prototype = {
     str.push("<div class='feed_main'>");
     $.each(msgs, function(ind, msg){
         var senderArrObj = $.grep(references, function(e){ return e.id == msg.sender_id; }),
-            senderName = (senderArrObj.length > 0) ? senderArrObj[0].full_name : "";
+            senderName = (senderArrObj.length > 0) ? senderArrObj[0].full_name : "",
+            senderPicURL = (senderArrObj.length > 0) ? senderArrObj[0].mugshot_url : "";
         str.push("<div class='msg_main'>");
+        str.push("<div class='msg_sender_pic'><img src='"+senderPicURL+"'/></div>");
+        str.push("<div class='msg_details_main'>");
         str.push("<div class='msg_head'>");
         str.push("<div class='msg_sender_name'>"+senderName+"</div>");
         str.push("<div class='msg_date_time'>"+msg.created_at+"</div>");
         str.push("</div>");
         str.push("<div class='msg_body'>");
         str.push(msg.body.plain);
+        str.push("</div>");
         str.push("</div>");
         str.push("</div>");
     });
