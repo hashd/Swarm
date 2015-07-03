41dd4a1291857e1ef460f18bf9d6074f3506768d
Manoj Kumar Padala <manoj.padala@imaginea.com>
2015-07-03 23:17:03 +0530

    changed date formats

--- js/feeds.js
+++ js/feeds.js
@@ -12,7 +12,7 @@ Feeds.prototype = {
   		type :"GET",
   		url : "https://www.yammer.com/api/v1/messages.json?access_token="+yammer.getAccessToken(),
       data:{
-        "limit":5
+        "limit":7
       },
   		dataType: 'json',
   		xhrFields: {
@@ -33,17 +33,33 @@ Feeds.prototype = {
         msgs = data.messages,
         references = data.references,
         str = [];
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+      "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
     str.push("<div class='feed_main'>");
     $.each(msgs, function(ind, msg){
         var senderArrObj = $.grep(references, function(e){ return e.id == msg.sender_id; }),
-            senderName = (senderArrObj.length > 0) ? senderArrObj[0].full_name : "",
-            senderPicURL = (senderArrObj.length > 0) ? senderArrObj[0].mugshot_url : "";
+        senderName = (senderArrObj.length > 0) ? senderArrObj[0].full_name : "",
+        senderPicURL = (senderArrObj.length > 0) ? senderArrObj[0].mugshot_url : "",
+        msgCreatedDate = msg.created_at;
+        var todayDate = new Date();
+        var msgDate = new Date(msgCreatedDate);
+        if(todayDate.getDate() == msgDate.getDate() &&
+            todayDate.getMonth() == msgDate.getMonth() &&
+            todayDate.getFullYear() == msgDate.getFullYear()) {
+          msgCreatedDate = msgCreatedDate.split(' ')[1];
+          msgCreatedDate = msgCreatedDate.split(':')[0]+":"+msgCreatedDate.split(':')[1];
+        }
+        else {
+          msgCreatedDate = monthNames[msgDate.getMonth()] + " " + msgDate.getDate();
+        }
+
         str.push("<div class='msg_main'>");
         str.push("<div class='msg_sender_pic'><img src='"+senderPicURL+"'/></div>");
         str.push("<div class='msg_details_main'>");
         str.push("<div class='msg_head'>");
         str.push("<div class='msg_sender_name'>"+senderName+"</div>");
-        str.push("<div class='msg_date_time'>"+msg.created_at+"</div>");
+        str.push("<div class='msg_date_time'>"+msgCreatedDate+"</div>");
         str.push("</div>");
         str.push("<div class='msg_body'>");
         str.push(msg.body.plain);
