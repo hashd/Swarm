82193075fabb2c690c0544629e65b84ec91cb63f
Manoj Kumar Padala <manoj.padala@imaginea.com>
2015-07-03 22:48:39 +0530

    updated code to show received messages

--- js/messages.js
+++ js/messages.js
@@ -3,6 +3,55 @@ function Messages(){
 };
 Messages.prototype = {
   init: function(){
-    alert("Messages");
-  }
+  	var self = this;
+  	self.getReceivedMessages();
+  },
+  getReceivedMessages: function(){
+      var self = this;
+      jQuery.ajax({
+  		type :"GET",
+  		url : "https://www.yammer.com/api/v1/messages/received.json?access_token="+yammer.getAccessToken(),
+      data:{
+        "limit":5
+      },
+  		dataType: 'json',
+  		xhrFields: {
+  			withCredentials: false
+  		},
+  		success : function(data){
+        self.buildFeedInfo(data);
+  			console.log(data);
+  		},
+  		error : function(){
+  			alert("error");
+  		}
+  	});
+  },
+  buildFeedInfo: function(data){
+    var self = this,
+        container = $("#content"),
+        msgs = data.messages,
+        references = data.references,
+        str = [];
+    str.push("<div class='feed_main'>");
+    $.each(msgs, function(ind, msg){
+        var senderArrObj = $.grep(references, function(e){ return e.id == msg.sender_id; }),
+            senderName = (senderArrObj.length > 0) ? senderArrObj[0].full_name : "",
+            senderPicURL = (senderArrObj.length > 0) ? senderArrObj[0].mugshot_url : "";
+        str.push("<div class='msg_main'>");
+        str.push("<div class='msg_sender_pic'><img src='"+senderPicURL+"'/></div>");
+        str.push("<div class='msg_details_main'>");
+        str.push("<div class='msg_head'>");
+        str.push("<div class='msg_sender_name'>"+senderName+"</div>");
+        str.push("<div class='msg_date_time'>"+msg.created_at+"</div>");
+        str.push("</div>");
+        str.push("<div class='msg_body'>");
+        str.push(msg.body.plain);
+        str.push("</div>");
+        str.push("</div>");
+        str.push("</div>");
+    });
+    str.push("</div>");
+    container.empty().html(str.join(''));
+  },
 }
