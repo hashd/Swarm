6d20d12742d65df87dfc3906d552848389184104
Thanigaivelu Mohanraj <mohanraj.t@imaginea.com>
2015-07-04 12:23:05 +0530

    Added link to profile page of the sender names in feeds

--- js/feeds.js
+++ js/feeds.js
@@ -12,19 +12,51 @@ Feeds.prototype = {
   		type :"GET",
   		url : "https://www.yammer.com/api/v1/messages.json?access_token="+yammer.getAccessToken(),
       data:{
-        "limit":7
+        "limit":5
       },
   		dataType: 'json',
   		xhrFields: {
   			withCredentials: false
   		},
   		success : function(data){
-        utils.buildFeedInfo(data);
-  			//console.log(data);
+        self.buildFeedInfo(data);
+  			console.log(data);
   		},
   		error : function(){
   			alert("error");
   		}
   	});
+  },
+  buildFeedInfo: function(data){
+    var self = this,
+        container = $("#content"),
+        msgs = data.messages,
+        references = data.references,
+        str = [];
+    str.push("<div class='feed_main'>");
+    $.each(msgs, function(ind, msg){
+        var senderArrObj = $.grep(references, function(e){ return e.id == msg.sender_id; }),
+            senderName = (senderArrObj.length > 0) ? senderArrObj[0].full_name : "",
+            senderPicURL = (senderArrObj.length > 0) ? senderArrObj[0].mugshot_url : "",
+            msgDateTime = new Date(msg.created_at)
+        str.push("<div class='msg_main'>");
+        str.push("<div class='msg_sender_pic'><a class='senderLinkAnc' href='javascript:{}'><img src='"+senderPicURL+"'/></a></div>");
+        str.push("<div class='msg_details_main'>");
+        str.push("<div class='msg_head'>");
+        str.push("<div class='msg_sender_name'><a class='senderLinkAnc' href='javascript:{}'>"+senderName+"</a></div>");
+        str.push("<div class='msg_date_time'>"+msg.created_at+"</div>");
+        str.push("</div>");
+        str.push("<div class='msg_body'>");
+        str.push(msg.body.plain);
+        str.push("</div>");
+        str.push("</div>");
+        str.push("</div>");
+    });
+    str.push("</div>");
+    container.empty().html(str.join(''));
+    container.off("click", ".feed_main a.senderLinkAnc").on("click", ".feed_main a.senderLinkAnc", function(){
+        var profileObj = new Profile();
+        profileObj.init();
+    });
   },
 }
