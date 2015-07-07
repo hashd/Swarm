c9b0f83ecfe8e4b59f1158f6c9cfe86434d92e56
hashd <gitk@danduprolu.com>
2015-07-07 19:49:19 +0530

    Material first cut

--- js/main.js
+++ js/main.js
@@ -1,58 +1,37 @@
-
-
 $(document).ready(function() {
     init();
 });
 
 function init(){
-  $('#nav li').hover(
-        function () {
-            $('ul', this).stop(true, true).slideDown(200);
-        },
-        function () {
-            $('ul', this).stop(true, true).slideUp(200);
-        }
-  );
-  $(document).find("div#navbar a.listAnc").click(function(event, isFirst){
+  $(document).find(".nav-bar i").click(function(event, isFirst){
     var target = $(this),
         jsVal = target.data("jsval"),
         jsValCap = jsVal.replace(/^[a-z]/, function(m){ return m.toUpperCase() });
         jsObj = loadJSObj(jsValCap);
         jsObj.init();
-        if(!isFirst){
-            $("a.menubtn").trigger("click");
 
-            chrome.storage.local.set({'newImagePath': 'yammerlogo_notifier.png'});
-            // create alarm for polling new messages every 1 minutes
-            chrome.alarms.create('checkNewTasks', {
-              when: 1000,
-              periodInMinutes: 1
-            });
+        if (!isFirst) {
+          chrome.storage.local.set({'newImagePath': 'yammerlogo_notifier.png'});
+          // create alarm for polling new messages every 1 minutes
+          chrome.alarms.create('checkNewTasks', {
+            when: 1000,
+            periodInMinutes: 1
+          });
         }
-        chrome.browserAction.setIcon({
-                    path: "yammerlogo.png"
-        });
-        target.parent().find('a').removeClass('selected');
-        target.addClass('selected');
+        chrome.browserAction.setIcon({ path: "yammerlogo.png" });
+        target.parent().siblings().find('i').removeClass('active');
+        target.addClass('active');
     });
 
-    var links = [{
-                        "bgcolor":"#386cbb",
-                        "icon":"+"
-                    }];
-    $('.kc_fab_wrapper').kc_fab(links);
-    $('.kc_fab_wrapper').find('button.kc_fab_main_btn').click(function(event) {
-      jsObj = loadJSObj("Postmessage");
-      jsObj.init();
-    });
-    $(document).find('input.search').change(function(){
+    /*$(document).find('input.search').change(function(){
         var target = $(this);
         var searchObj = new Search();
         searchObj.init(target.val());
-    });
+    });*/
 
-  $($("a.listAnc").get(0)).trigger("click", [true]);
+  $(".nav-bar i").first().trigger("click", [true]);
 };
+
 function loadJSObj(jsVal){
   var jsObj = "";
   switch (jsVal){
