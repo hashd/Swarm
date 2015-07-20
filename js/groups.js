fc14bc17201990bac67da3112d64434cf36eed74
Krishnam <krishnam.g@imaginea.com>
2015-07-20 23:25:33 +0530

    adding post to all company feature

--- js/groups.js
+++ js/groups.js
@@ -4,60 +4,68 @@ Swarm.Groups = function (){
 
 Swarm.Groups.prototype = {
   init: function(){
-  	var self = this;
-  	self.buildGroupMessageFeed();
+    var self = this;
+    self.buildGroupMessageFeed();
   },
 
   buildGroupMessageFeed : function(){
-  	var self = this;
-  	container = $("#content"),
-  	jQuery.ajax({
-  		type :"GET",
-  		url : "https://www.yammer.com/api/v1/users/current.json?access_token="+yammer.getAccessToken()+"&include_group_memberships=true",
-      	data:{
-        	"limit":1
-      	},
-  		dataType: 'json',
-  		xhrFields: {
-  			withCredentials: false
-  		},
-  		success : function(data){
-			container.empty().html(Swarm.templates.groups({ groups: data}));
-			self.displayGroupMessages();
-			$('#slt_groups').trigger('change');
-  		},
-  		error : function(){
-  			alert("error");
-  		}
-  	});
+    var self = this;
+    container = $("#content"),
+    jQuery.ajax({
+      type :"GET",
+      url : "https://www.yammer.com/api/v1/users/current.json?access_token="+yammer.getAccessToken()+"&include_group_memberships=true",
+        data:{
+          "limit":1
+        },
+      dataType: 'json',
+      xhrFields: {
+        withCredentials: false
+      },
+      success : function(data){
+        container.empty().html(Swarm.templates.groups({ groups: data}));
+        self.addAllCompany();
+        self.displayGroupMessages();
+        $('#slt_groups').trigger('change');
+      },
+      error : function(){
+        alert("error");
+      }
+    });
+  },
+
+  addAllCompany : function(){
+    $("#slt_groups").prepend("<option value='all' selected='selected'>All Company</option>");
   },
 
   displayGroupMessages :function() {
-  	var self = this;
-	  $('#slt_groups').change(function() {
-		var content = $("#content");
-		var groupId = $("select#slt_groups").val();
-		
-		jQuery.ajax({
-  		type :"GET",
-      
-  		url : 'https://www.yammer.com/api/v1/messages/in_group/'+groupId+'.json?access_token='+yammer.getAccessToken(),
-      	data:{
-        	"limit":20
-      	},
-  		dataType: 'json',
-  		xhrFields: {
-  			withCredentials: false
-  		},
-  		success : function(data){
-  			content.find('div.feed_main').remove();
-        	Swarm.utils.buildFeedInfo(data);
-  		},
-  		error : function(){
-  			alert("error");
-  		}
-  		});
+    var self = this;
+    $('#slt_groups').change(function() {
+    var content = $("#content");
+    var groupId = $("select#slt_groups").val();
+    var allCompanyUrl = "https://www.yammer.com/api/v1/messages/general.json";
+    var url = 'https://www.yammer.com/api/v1/messages/in_group/'+groupId+'.json?access_token='+yammer.getAccessToken();
+    if(groupId == "all"){
+      url = allCompanyUrl;
+    }
+    jQuery.ajax({
+      type :"GET",
+      url : url,
+        data:{
+          "limit":20
+        },
+      dataType: 'json',
+      xhrFields: {
+        withCredentials: false
+      },
+      success : function(data){
+        content.find('div.feed_main').remove();
+          Swarm.utils.buildFeedInfo(data);
+      },
+      error : function(){
+        alert("error");
+      }
+      });
 
-	});
+  });
   },
 }
