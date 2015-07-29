dca05a0b4bfcf0a673ecb0c267e36c3386465281
hashd <gitk@danduprolu.com>
2015-07-29 21:45:27 +0530

    Tweaked back icon to a slightly better position

--- js/api.js
+++ js/api.js
@@ -97,7 +97,7 @@ Swarm.API.prototype = {
     var self = this,
     mugshotContainer = $('.header .current-mugshot'),
     content = $('#content');
-    mugshotContainer.html('<i class="material-icons" style="font-size:1.5em;cursor:pointer">arrow_back</i>');
+    mugshotContainer.html('<i class="material-icons" style="font-size:1.5em;cursor:pointer;padding:6px 3px;">arrow_back</i>');
   },
 
   /** All Yammer API Calls below */
